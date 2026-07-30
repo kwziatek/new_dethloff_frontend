bfcc6372b8185d1b0f5d2c5b9df8c5eab6cadc27
Karol Wziatek <wziatek.karol04@gmail.com>
2026-07-30 17:39:51 +0200

    implementation of rediraction to root page when token expired or is invalid & finishing filter bar and adding some styles on students page

--- src/js/auth-check.js
+++ src/js/auth-check.js
@@ -1,9 +1,33 @@
-// page-auth check preventing unlogged user from seeing contect only for logged users
-const PUBLIC_PAGES = ['/', '/pages/contact', '/pages/login', 'pages/aboutSchool'];
+// IMPORTANT: Since we want to execute this script before the HTML page loads, we can't use type="module". Because of that importing any module is impossible, that's why fetch API is used instead of axios.
+
+// page-auth check preventing unlogged user or one with invalid token from seeing contect only for logged users
+const PUBLIC_PAGES = ['/', '/pages/contact', '/pages/login', '/pages/aboutSchool'];
 
 const currentPath = window.location.pathname;
 const token = localStorage.getItem('jwt_token');
 
-if (!PUBLIC_PAGES.includes(currentPath) && !token) {
+function redirectToLogin(error) {
+    console.error("Session expired or invalid token: ", error);
+    localStorage.removeItem('jwt_token');
     window.location.replace('/');
+}
+
+if(!PUBLIC_PAGES.includes(currentPath)) {
+    if (!token) {
+        redirectToLogin();
+    } else {
+        fetch("http://localhost:8080/api/auth/me", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => {
+            if(!res.ok) {
+                redirectToLogin(res);
+            }
+        })
+        .catch((error => {
+            redirectToLogin(error);
+        }));
+    }
 }
--- src/js/students.js
+++ src/js/students.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const listOfStudentsSpace = document.querySelector(".setOfStudents");
+const filterBar = document.querySelector("#filterBar");
+
 try {
     const auth = {
         headers: {
@@ -7,9 +10,36 @@ try {
         }
     }
     const response = await axios.get('http://localhost:8080/api/students', auth);
-    //how should the jwt_token be sent to BE?
-    console.log(response.data);
+    const data = response.data;
+    data.forEach(element => {
+        const newA = document.createElement("a");
+        const newP = document.createElement("p");
+        newP.classList.add('student');
+        newP.innerHTML = element.name + " " + element.surname;
+        newA.href = "/pages/students/" + element.id;
+        newA.appendChild(newP);
+        listOfStudentsSpace.append(newA);
+    });
     
 } catch(e) {
     alert(e.message);
 }
+
+filterBar.addEventListener("input", (e) => {
+    const userInput = e.target.value.toLowerCase();
+    let matchCount = 0;
+    
+    Array.from(listOfStudentsSpace.children).forEach(student => {
+        const nameSurname = student.querySelector('.student').textContent.toLowerCase();
+        const isMatch = nameSurname.includes(userInput)
+
+        if(isMatch && matchCount < 30) {
+            student.style.display = ""; // Restores standard CSS layout
+            matchCount++;
+        } else {
+            student.style.display = "none";
+        }
+    });
+
+});
+
