f5016e1458e0c055a6ac79805b4a482c30ccdd98
Karol Wziatek <wziatek.karol04@gmail.com>
2026-08-03 22:21:25 +0200

    limiting the number of displayed students

--- src/js/students.js
+++ src/js/students.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const listOfStudentsSpace = document.querySelector(".setOfStudents");
 const filterBar = document.querySelector("#filterBar");
+const availableSpace = 16;
 
 const fetchStudents = async () => {
     try {
@@ -12,6 +13,7 @@ const fetchStudents = async () => {
         }
         const response = await axios.get('http://localhost:8080/api/students', auth);
         const data = response.data;
+        let shownCount = 0;
         data.forEach(element => {
             const newA = document.createElement("a");
             const newP = document.createElement("p");
@@ -20,6 +22,11 @@ const fetchStudents = async () => {
             newA.href = "/pages/students/" + element.id;
             newA.classList.add('studentAnchor');
             newA.appendChild(newP);
+            if(shownCount >= availableSpace) {
+                newA.style.display = "none";
+            } else {
+                shownCount++;
+            }
             listOfStudentsSpace.append(newA);
         });
     } catch(e) {
@@ -36,7 +43,7 @@ const enableFilterBar = async () => {
             const nameSurname = student.querySelector('.student').textContent.toLowerCase();
             const isMatch = nameSurname.includes(userInput)
 
-            if(isMatch && matchCount < 30) {
+            if(isMatch && matchCount < availableSpace) {
                 student.style.display = ""; // Restores standard CSS layout
                 matchCount++;
             } else {
