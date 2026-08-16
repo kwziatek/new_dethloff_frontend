62d2e7beaf7db9290f7bf5c6b877076b8429b306
Karol Wziatek <wziatek.karol04@gmail.com>
2026-08-16 16:23:14 +0200

    started the course page, courses are being fetch from BE API and displayed

---
+++ src/js/courses.js
@@ -0,0 +1,100 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const api = axios.create({baseURL: API_URL});
+
+const listOfCoursesSpace = document.querySelector("#setOfAllCourses");
+// const filterBar = document.querySelector("#filterBar");
+const availableSpace = 32;
+const modal = document.getElementById("courseModal");
+const form = document.getElementById("courseForm");
+
+const fetchCourses = async () => {
+    try {
+        const auth = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
+            }
+        }
+        const response = await api.get('/api/courses', auth);
+        const data = response.data;
+        let shownCount = 0;
+        data.forEach(element => {
+            const newA = document.createElement("a");
+            const newP = document.createElement("p");
+            newP.classList.add('course');
+            newP.innerHTML = element.name + "<br>" + element.teacher.name + " " + element.teacher.surname;
+            newA.href = "/pages/courseDetails" + "?id=" + element.id;
+            newA.classList.add('courseAnchor');
+            newA.appendChild(newP);
+            if(shownCount >= availableSpace) {
+                newA.style.display = "none";
+            } else {
+                shownCount++;
+            }
+            console.log(listOfCoursesSpace);
+            listOfCoursesSpace.append(newA);
+        });
+    } catch(e) {
+        alert(e.message);
+    };
+};
+
+// const enableFilterBar = async () => {
+//     filterBar.addEventListener("input", (e) => {
+//         const userInput = e.target.value.toLowerCase(); 
+//         let matchCount = 0;
+        
+//         Array.from(listOfStudentsSpace.children).forEach(student => {
+//             const nameSurname = student.querySelector('.student').textContent.toLowerCase();
+//             const isMatch = nameSurname.includes(userInput)
+
+//             if(isMatch && matchCount < availableSpace) {
+//                 student.style.display = ""; // Restores standard CSS layout
+//                 matchCount++;
+//             } else {
+//                 student.style.display = "none";
+//             }
+//         });
+//     });
+// };
+
+const addCourseButtonAction = async () => {
+    document.getElementById("createCourse").addEventListener("click", () => {
+        form.reset();
+        modal.showModal();
+    });
+}
+
+const submitButtonAction = async () => {
+    form.addEventListener("submit", async (e) => {
+        // e.preventDefault();
+
+        const formData = new FormData(form);
+        const payload = Object.fromEntries(formData);
+
+        const auth = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("jwt_token")}`
+            }
+        };
+        try {
+            const response = await api.post("/api/courses", payload, auth);
+            const data = await response.data;
+            window.location.href = `/pages/courseDetails?id=${data.id}`;
+            modal.close();
+        } catch (error) {
+            console.error("Failed to add course: ", error);
+        }
+        
+    });
+}
+
+const workflow = async () => {
+    await fetchCourses();
+    // enableFilterBar();
+    addCourseButtonAction();
+    submitButtonAction();
+};
+
+workflow();
