f950eba0cf9067f6a4f30350dad865c67d6f5fba
Karol Wziatek <wziatek.karol04@gmail.com>
2026-09-21 22:57:48 +0200

    further progress on course details page - enroll student button display student input and students list, which updates accordingly to input

--- src/js/courseDetails.js
+++ src/js/courseDetails.js
@@ -1,28 +1,43 @@
 import axios from "axios";
-import { showToast, setRedirectToast } from "./global";
+import {
+  showToast,
+  setRedirectToast,
+  createNameSurnameDropdownListElements,
+} from "./global";
 
 const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({ baseURL: API_URL });
 
 const courseId = new URLSearchParams(window.location.search).get("id");
 
+const allStudents = [];
+
 const standardContainer = document.querySelector(".standard-container");
 const leftContainer = document.querySelector(".left-container");
+const mainSection = leftContainer.querySelector(".main-section");
 const rightContainer = document.querySelector(".right-container");
+const auth = {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
+  },
+};
 
 const fetchCourseDetails = async () => {
-  const auth = {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
-    },
-  };
-
   try {
     const courseData = await api.get(`/api/courses/${courseId}`, auth);
     return courseData.data;
   } catch (e) {
     showToast("Błąd serwera", "error");
     console.log(e);
+  }
+};
+
+const fetchAllStudents = async () => {
+  try {
+    const studentsData = await api.get(`/api/students`, auth);
+    return studentsData.data;
+  } catch (e) {
+    showToast("Błąd serwera", "error");
   }
 };
 
@@ -49,7 +64,6 @@ const fillEsstentialData = (courseData) => {
 };
 
 const fillEnrolledStudents = (courseData) => {
-  const mainSection = leftContainer.querySelector(".main-section");
   const studentsCount = leftContainer.querySelector("#students-count");
   studentsCount.innerText = courseData.students.length;
 
@@ -65,10 +79,63 @@ const fillPageWithCourseData = (courseData) => {
   fillEnrolledStudents(courseData);
 };
 
+const addEnrollStudentButtonAction = (studentInput, studentsDropdown) => {
+  const enrollButton = leftContainer.querySelector("#enroll-student-button");
+
+  studentInput.addEventListener("input", (e) => {
+    const query = e.target.value.toLowerCase().trim();
+
+    Array.from(mainSection.querySelectorAll(".studentLI")).forEach(
+      (studentLI) => {
+        const text = studentLI.querySelector("p").innerText.toLowerCase();
+
+        if (text.includes(query)) {
+          studentLI.style.display = "block";
+        } else {
+          studentLI.style.display = "none";
+        }
+      },
+    );
+  });
+
+  enrollButton.addEventListener("click", (e) => {
+    const isHidden = studentInput.style.display;
+    if (isHidden) {
+      studentInput.style.display = "block";
+      studentsDropdown.style.display = "block";
+    } else {
+      studentInput.style.display = "none";
+      studentsDropdown.style.display = "none";
+    }
+  });
+};
+
+const addOtherHTMLElements = () => {
+  const studentInput = document.createElement("input");
+  studentInput.style.display = "none";
+  mainSection.appendChild(studentInput);
+
+  const studentsDropdown = document.createElement("ul");
+  studentsDropdown.classList.add("dropdown-list");
+  mainSection.appendChild(studentsDropdown);
+  return [studentInput, studentsDropdown];
+};
+
 const loadPageContent = async () => {
-  const courseData = await fetchCourseDetails();
-  console.log(courseData);
+  const [courseData, studentsData] = await Promise.all([
+    fetchCourseDetails(),
+    fetchAllStudents(),
+  ]);
+
   fillPageWithCourseData(courseData);
+  const [studentInput, studentsDropdown] = addOtherHTMLElements();
+
+  addEnrollStudentButtonAction(studentInput, studentsDropdown);
+  createNameSurnameDropdownListElements(
+    studentsData,
+    "studentLI",
+    studentsDropdown,
+  ); //create students dropdown in html
 };
 
 const workflow = () => {
--- src/js/global.js
+++ src/js/global.js
@@ -2,42 +2,42 @@ const logout = document.querySelector("#logOut");
 const goBack = document.querySelector("#goBack");
 
 if (logout) {
-    logout.addEventListener("click", (e) => {
-        localStorage.removeItem("jwt_token");
-        setRedirectToast("Wylogowano", "info");
-        window.location.href = "login";
-    });
+  logout.addEventListener("click", (e) => {
+    localStorage.removeItem("jwt_token");
+    setRedirectToast("Wylogowano", "info");
+    window.location.href = "login";
+  });
 }
 
 if (goBack) {
-    goBack.addEventListener("click", (e) => {
-        window.history.back();
-    });
+  goBack.addEventListener("click", (e) => {
+    window.history.back();
+  });
 }
 
 export const showToast = (message, type = "success", duration = 3000) => {
-    let toastContainer = document.querySelector("#toast-container");
-    if(!toastContainer) {
-        toastContainer = document.createElement("div");
-        toastContainer.id = "toast-container";
-        document.body.appendChild(toastContainer);
-    } 
-    let toast = document.createElement("div");
-    toast.textContent = message;
-    toast.className =`toast ${type}`;
-    toastContainer.appendChild(toast);
+  let toastContainer = document.querySelector("#toast-container");
+  if (!toastContainer) {
+    toastContainer = document.createElement("div");
+    toastContainer.id = "toast-container";
+    document.body.appendChild(toastContainer);
+  }
+  let toast = document.createElement("div");
+  toast.textContent = message;
+  toast.className = `toast ${type}`;
+  toastContainer.appendChild(toast);
 
-    setTimeout(() => toast.remove(), duration);
-}
+  setTimeout(() => toast.remove(), duration);
+};
 
 const toastWorkflow = () => {
-    const toastData = sessionStorage.getItem("redirect_toast");
-    if(toastData) {
-        const {message, type} = JSON.parse(toastData);
-        showToast(message, type);
-        sessionStorage.removeItem("redirect_toast");
-    }   
-}
+  const toastData = sessionStorage.getItem("redirect_toast");
+  if (toastData) {
+    const { message, type } = JSON.parse(toastData);
+    showToast(message, type);
+    sessionStorage.removeItem("redirect_toast");
+  }
+};
 
 toastWorkflow();
 
@@ -47,7 +47,27 @@ toastWorkflow();
 // Utility methods can also be found in common page logic
 
 export const setRedirectToast = (message, type) => {
-    sessionStorage.setItem("redirect_toast", JSON.stringify( {message, type }));
-}
+  sessionStorage.setItem("redirect_toast", JSON.stringify({ message, type }));
+};
+
+export const createNameSurnameDropdownListElements = (
+  nameSurnameDataArray,
+  classNameForLI,
+  dropdown,
+) => {
+  nameSurnameDataArray.forEach((personData) => {
+    //data paragraph
+    const dataParagraph = document.createElement("p");
+    dataParagraph.innerHTML = personData.name + " " + personData.surname;
+    dataParagraph.dataset.personId = personData.id;
+    //list element
+    const listElement = document.createElement("li");
+    listElement.classList.add(classNameForLI);
+    listElement.style.display = "none";
+    listElement.appendChild(dataParagraph);
+    //append to dropdown
+    dropdown.appendChild(listElement);
+  });
+};
 
 // -- Utility methods ------------------------------------------------------------------------------------------------------------------
